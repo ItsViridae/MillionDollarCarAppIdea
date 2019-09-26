eb5a11a57ccd736ed0044858a9228ba181ba3744
Scott Comeaux <viridaecode@gmail.com>
2019-09-25 19:41:57 -0500

    feat - 2-Letter State Validation
    Checks to see if States are real states according to Postal regulations

---
+++ FA19.P05.Web/Features/Shared/StateValidation.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FA19.P05.Web.Features.Shared
+{
+    public class StateValidation
+    {
+        public List<string> StateAbbrevs = new List<string>
+        {
+            "AL", "AK", "AS", "AZ", "AR", "CA", "CO", "CT", "DE", "DC", "FM", "FL", "GA", "GU", "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MH", "MD", "MA", "MI", "MN", "MS", "MT", "MO", "NE", "NV", "NH", "NJ",  "NM", "NY", "NC", "ND", "MP", "OH", "OK", "OR", "PW", "PA", "PR", "RI", "SC", "SD", "TN", "TX", "UT", "VT", "VI", "VA", "WA", "WV", "WI", "WY"
+        };
+    }
+}
