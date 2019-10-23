9726b40ca4e4d1f67bf26dd01af780847eb9d210
Scott Comeaux <viridaecode@gmail.com>
2019-10-22 23:53:51 -0500

    Fixed routes in SignedInLinks.js
    Correctly routes to different pages apon Click.

--- FA19.P05.Web/ClientApp/million-dollar-dealership/src/component/Layout/SignedInLinks.js
+++ FA19.P05.Web/ClientApp/million-dollar-dealership/src/component/Layout/SignedInLinks.js
@@ -4,8 +4,8 @@ import { NavLink } from 'react-router-dom'
 const SignedInLinks = () => {
     return (
         <ul className="right">
-            <li><NavLink to='/'>Inventory</NavLink></li>
-            <li><NavLink to='/'>Users</NavLink></li>
+            <li><NavLink to='/inventory'>Inventory</NavLink></li>
+            <li><NavLink to='/users'>Users</NavLink></li>
             <li><NavLink to='/login'>Logout</NavLink></li>
             {/*<li><NavLink to='/' className='btn btn-floating black lighten-1'>BL</NavLink></li>*/}
         </ul>
