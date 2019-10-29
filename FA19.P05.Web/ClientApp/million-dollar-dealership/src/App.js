e11d92bdd7870b70dee76115e807cdc95391a7aa
Brian <brian.lark@selu.edu>
2019-10-29 08:36:36 -0500

    Render Login Page
    Handle Logout Controller

--- FA19.P05.Web/ClientApp/million-dollar-dealership/src/App.js
+++ FA19.P05.Web/ClientApp/million-dollar-dealership/src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Route, Switch } from "react-router-dom";
-import Navbar from "./component/Layout/Navbar";
+import Navbar from "./component/Layout/Navbar"
 import Signup from "./component/Authentication/Signup";
 import Login from "./component/Authentication/Login";
 
@@ -8,15 +8,17 @@ function App() {
 
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+
   return (
     <div className="App">
-      <Navbar isLoggedIn={isLoggedIn}/>
+      <Navbar isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />
       <Switch>
         <Route path="/signup" component={Signup} />
         <Route
           path="/login"
           render={() => (
             <Login
+              isLoggedIn={isLoggedIn}
               setIsLoggedIn={setIsLoggedIn}
             />
           )}
--- FA19.P05.Web/ClientApp/million-dollar-dealership/src/component/Layout/SignedInLinks.js
+++ FA19.P05.Web/ClientApp/million-dollar-dealership/src/component/Layout/SignedInLinks.js
@@ -1,12 +1,29 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
+import axios from 'axios'
 
-const SignedInLinks = () => {
+const SignedInLinks = ({ setIsLoggedIn }) => {
+
+    const handleLogout = (e) => {
+
+        axios
+            .post("/api/Logout")
+            .then(function(response) {
+                if (response.status === 200) {
+                setIsLoggedIn(false);
+                }
+                console.log(response);
+            })
+            .catch(function(error) {
+                console.log(error);
+            });
+        }
+
     return (
         <ul className="right">
             <li><NavLink to='/'>Inventory</NavLink></li>
             <li><NavLink to='/'>Users</NavLink></li>
-            <li><NavLink to='/login'>Logout</NavLink></li>
+            <li><NavLink to='/login' onClick={handleLogout}>Logout</NavLink></li>
             {/*<li><NavLink to='/' className='btn btn-floating black lighten-1'>BL</NavLink></li>*/}
         </ul>
     )
